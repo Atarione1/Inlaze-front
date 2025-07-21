1f33c55a15d430065094f908aae2f68eb8ae418b
Ataone11 <atari105@gmail.com>
2025-07-21 13:07:30 -0500

    error

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
-import { getAdmin, getProject } from "@/app/api/services/service";
+import { getAdmin, getProject, getUsers } from "@/app/api/services/service";
 import Link from "next/link";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { getTasks, getTaskss, getUserTask } from "@/app/api/services/serviceTasks";
@@ -44,9 +44,10 @@ const Project = () => {
     const addmin = await getAdmin(session)
     const project = await getProject(session, params?.id as string)
     const userr = await getUserTask(session, newUser)
+    const users = await getUsers(session)
     setAdmin(addmin)
     setProject(project)
-    setUser(userr)
+    setUser(users)
 
 
     const taskss = await getTaskss(session, params?.id as string)
@@ -92,7 +93,16 @@ const Project = () => {
                   </div>
 
                   <div className=" flex items-center gap-2">
-                    <div className=" ">Usuario: {pro.userId} </div>
+                    {user?.map((user: any, i) => (
+                      <div key={i}>
+                        {user.id == pro.userId ? <div>
+                          <p className="text-sm text-gray-600 …">
+                            <div className=" ">Asignado a: {user.name} </div>
+                          </p>
+                        </div> : <div></div>}
+                      </div>
+                    ))}
+
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 text-slate-500 hover:text-slate-700 hover:cursor-pointer">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
